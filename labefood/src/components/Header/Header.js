45cce1221eada70c75d93f3d42ecb8a9a95a08e7
Liz alves <elisabete_carente@hotmail.com>
2022-07-29 08:45:57 -0300

    ajustando as paginas assets e components

--- labefood/src/components/Header/Header.js
+++ labefood/src/components/Header/Header.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import backIcon from '../../assets/back_arrow.svg'
 import { useNavigate } from 'react-router-dom'
-import { HeaderMainDiv } from './styled'
-import { goBack } from '../../routes/coodinator'
+import { HeaderMainDiv } from './styled';
+import { goToLastPage } from '../../routes/cordinator';
 
 const Header = (props) => {
-   const navigate = useNavigate()
+  const navigate = useNavigate(); 
 
-   return (
+  return (
 
-      <HeaderMainDiv>
-         {props.hideBackButton ? <span id='spacingLeft'></span> : <img alt='back icon' src={backIcon} onClick={() => goBack(navigate)} />}
-         <p>{props.title && props.title.length > 0 ? props.title : null}</p>
-         <span id='spacingRight'></span>
-      </HeaderMainDiv>
-   )
+    <HeaderMainDiv>
+        {props.hideBackButton ? <span id='spacingLeft'></span>:<img alt='back icon' src={backIcon} onClick={() => goToLastPage(navigate)}/> }
+        <p>{props.title && props.title.length > 0? props.title: null }</p>
+        <span id='spacingRight'></span>
+    </HeaderMainDiv>
+        
+  
+  )
 }
 
 export default Header
-
--- labefood/src/components/NavigationBar/NavigationBar.js
+++ labefood/src/components/NavigationBar/NavigationBar.js
@@ -7,24 +7,27 @@ import shoppingCartW from '../../assets/footerImg/shopping-cart-w.png'
 import shoppingCart from '../../assets/footerImg/shopping-cart.png'
 import { useNavigate } from 'react-router-dom'
 import { IconButton } from '@material-ui/core'
-import { goToCartPage, goToFeedPage, goToProfilePage } from '../../routes/coodinator'
+import { goToHome, goToCart, goToProfile } from '../../routes/cordinator'
 import { ContainerFooter } from "./styled"
 
-export const Footer = (props) => {
-   const navigate = useNavigate()
-   return (
-      <ContainerFooter>
-         <IconButton title={"Página principal"} onClick={() => goToFeedPage(navigate)} aria-label="home">
+
+const NavigationBar = (props) => {
+  const navigate = useNavigate();
+  return (
+    <ContainerFooter>
+         <IconButton title={"Página principal"} onClick={() => goToHome(navigate)} aria-label="home">
             <img src={props.page === 'home' ? homePage : homePageW} alt={'Imagem de uma casa, represwentando a página principal'} />
          </IconButton>
 
-         <IconButton title={"Carrinho"} onClick={() => goToCartPage(navigate)} aria-label="cart">
-            <img src={props.page === 'cart' ? shoppingCart : shoppingCartW} alt={'Imagem de um carrinho de compras'} />
+         <IconButton title={"Carrinho"} onClick={() => goToCart(navigate)} aria-label="cart">
+            <img src={props.sourcePage === 'cart' ? shoppingCart : shoppingCartW} alt={'Imagem de um carrinho de compras'} />
          </IconButton>
 
-         <IconButton title={"Perfil"} onClick={() => goToProfilePage(navigate)} aria-label="profile">
+         <IconButton title={"Perfil"} onClick={() => goToProfile(navigate)} aria-label="profile">
             <img src={props.page === 'profile' ? avatar : avatarW} alt={'Imagem ilustrativa representando o perfil'} />
          </IconButton>
       </ContainerFooter>
-   )
-}
+  );
+};
+
+export default NavigationBar;
